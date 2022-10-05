fd0a48f9fec5daf6862ab80430b4bf8f4648efe5
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-10-04 23:18:32 -0700

    Ejercicio 1 terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import Induccion from "./Induccion";
 import Nav from "./Nav";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Ejercicio1 from "./Ejercicio1";
+import Ejercicio2 from "./Ejercicio2";
+import Ejercicio3 from "./Ejercicio3";
+import Ejercicio4 from "./Ejercicio4";
+import Ejercicio5 from "./Ejercicio5";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -13,6 +18,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Induccion />} />
           <Route path="/Ejercicio1" element={<Ejercicio1 />} />
+          <Route path="/Ejercicio2" element={<Ejercicio2 />} />
+          <Route path="/Ejercicio3" element={<Ejercicio3 />} />
+          <Route path="/Ejercicio4" element={<Ejercicio4 />} />
+          <Route path="/Ejercicio5" element={<Ejercicio5 />} />
         </Routes>
       </Router>
     </div>
